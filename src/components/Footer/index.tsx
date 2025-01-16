666ca5e6f9421a210b8509feba6abfb984c2be40
ThaoVy <thaovyluuthanh@gmail.com>
2025-01-16 18:37:10 +0700

    update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,17 +10,14 @@ import Youtube from "@/assets/icons/Youtube.svg"
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-10">
-      <div className="container flex mx-auto px-4 sm:px-6 lg:px-8">
-        {/* Logo and Navigation */}
-        <div className="flex flex-col md:flex-row justify-between items-center border-b border-gray-700 pb-6 mb-6">
-          {/* Logo */}
+      <div className="container flex gap-8 mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col md:flex-row justify-between items-center  border-0 pb-6 mb-6">
           <div className="mb-6 md:mb-0">
             <span>
               <Image src={LogoWhite} alt="Logo" className="h-12 w-auto" />
             </span>
 
-            {/* Navigation */}
-            <nav className="text-center">
+            <nav className="text-center mt-14">
               <ul className="flex flex-wrap gap-4 text-sm text-gray-400">
                 <li>
                   <a href="#home" className="hover:text-white">
@@ -57,50 +54,54 @@ const Footer = () => {
           </div>
         </div>
 
-        {/* Contact Information */}
-        <div className="flex flex-col md:flex-row justify-between items-start text-sm text-gray-400">
+        <div className="flex flex-wrap text-sm text-gray-400">
           <div>
-            <div>
-              <p className="mb-4 font-semibold">CONTACT US</p>
-              <p>+02 8006 3344</p>
-            </div>
-
-            <div className="text-center mb-6 md:mb-0">
-              <p className="mb-4 font-semibold">EMAIL</p>
-              <p>robert@cosmedixclinics.com.au</p>
-            </div>
+            <p className="mb-4" style={{ fontSize: 12 }}>
+              CONTACT US
+            </p>
+            <p className="font-semibold text-2xl">+02 8006 3344</p>
           </div>
 
-          <div>
-            <div>
-              <p className="mb-4 font-semibold">ADDRESS</p>
-              <p>169 William Street - Darlinghurst</p>
-              <p>Sydney, NSW</p>
-            </div>
-
-            <div className="text-center">
-              <p className="mb-4 font-semibold">OPENING HOURS</p>
-              <p>9am—6pm</p>
-            </div>
-
-            <div className="flex space-x-4">
-              <a href="#" className="hover:text-white">
-                <Image src={Facebook} alt="Facebook" className="h-4 w-auto" />
-              </a>
-              <a href="#" className="hover:text-white">
-                <Image src={Instagram} alt="Instagram" className="h-4 w-auto" />
-              </a>
-              <a href="#" className="hover:text-white">
-                <Image src={Tiktok} alt="Tiktok" className="h-4 w-auto" />
-              </a>
-              <a href="#" className="hover:text-white">
-                <Image src={Youtube} alt="Youtube" className="h-4 w-auto" />
-              </a>
-            </div>
+          <div className="mb-6 md:mb-0">
+            <p className="mb-4" style={{ fontSize: 12 }}>
+              EMAIL
+            </p>
+            <p className="font-semibold">robert@cosmedixclinics.com.au</p>
           </div>
+        </div>
+        <div className="flex flex-wrap flex-col justify-between text-sm text-gray-400">
+          <div>
+            <p className="mb-4" style={{ fontSize: 12 }}>
+              ADDRESS
+            </p>
+            <p className="font-semibold">169 William Street - Darlinghurst</p>
+            <p className="font-semibold">Sydney, NSW</p>
+          </div>
+          <div>
+            <p className="mb-4" style={{ fontSize: 12 }}>
+              OPENING HOURS
+            </p>
+            <p className="font-semibold text-2xl">9am—6pm</p>
+          </div>
+          <div className="flex space-x-4 h-4">
+            <a href="#" className="hover:text-white">
+              <Image src={Facebook} alt="Facebook" className="h-4 w-auto" />
+            </a>
+            <a href="#" className="hover:text-white">
+              <Image src={Instagram} alt="Instagram" className="h-4 w-auto" />
+            </a>
+            <a href="#" className="hover:text-white">
+              <Image src={Tiktok} alt="Tiktok" className="h-4 w-auto" />
+            </a>
+            <a href="#" className="hover:text-white">
+              <Image src={Youtube} alt="Youtube" className="h-4 w-auto" />
+            </a>
+          </div>
+        </div>
 
-          <div className="mt-6 flex justify-between items-center text-gray-400 text-sm">
-            <div>© 2024 — COPYRIGHT</div>
+        <div className="flex flex-wrap flex-col-reverse text-gray-400">
+          <div style={{ fontSize: 8, textWrap: "nowrap" }}>
+            © 2024 — COPYRIGHT
           </div>
         </div>
       </div>
